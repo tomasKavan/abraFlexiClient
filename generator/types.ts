0d6dcff19080f3e6c8c6e4adfff746280cbaaa3e
Jan P <79443348+jpalenik@users.noreply.github.com>
2025-10-31 19:57:04 +0100

    revert generator (#3)
    * revert generator
    
    * rm tsconfig fix
    
    ---------
    
    Co-authored-by: = <=>

---
+++ generator/types.ts
@@ -0,0 +1,106 @@
+export interface EvidenceDef {
+  evidenceName: string,
+  evidenceType: string,
+  evidencePath: string,
+  formCode: string,
+  beanKey: string,
+  className: string,
+  extIdSupported: boolean,
+  dbName: string,
+
+  tsClassName: string
+}
+
+export enum PropertyType {
+  Integer = 'integer',
+  String = 'string',
+  Select = 'select',
+  DateTime = 'datetime',
+  Date = 'date',
+  Numeric = 'numeric',
+  Logic = 'logic',
+  Relation = 'relation',
+  Blob = 'blob',
+  Array = 'array'
+}
+
+export type EnumValue = {
+  "@key": string,
+  "$": string
+}
+
+export type ValueObj = {
+  value: EnumValue[]
+}
+
+export interface PropertyDef {
+  propertyName: string,
+  dbName: string,
+  name: string,
+  title: string,
+  type: PropertyType,
+  typeName?: string,
+
+  // Visibility
+  isVisible: boolean,
+  isSortable: boolean,
+  isHighlight: boolean,
+  inId: boolean,
+  inSummary: boolean,
+  inDetail: boolean,
+  inExpensive: boolean,
+
+  // Behavioar
+  isMassUpdateable: boolean,
+  mandatory: boolean,
+  isWritable: boolean,
+  isOverWritable: boolean,
+  hasBusinessLogic: boolean
+
+  // Other
+  isUpperCase: boolean,
+  isLowerCase: boolean,
+  links: null | {
+    link: string
+  }
+
+  // Values
+  importableValues?: ValueObj,
+  values?: ValueObj,
+  digits?: number,
+  decimals?: number,
+  maxLength?: number,
+  enumName?: string,
+  itemType?: PropertyType,
+
+  // to 1 relations
+  fkName?: string,
+  fkEvidencePath?: string,
+  fkEvidenceType?: string,
+  url?: string,
+  tsClassName?: string,
+
+  genType?: string
+}
+
+export interface RelationDef {
+  url: string,
+  evidenceType: string,
+  name: string,
+
+  tsClassName: string,
+  relationName: string,
+  getter?: string | null
+}
+
+export type EnumOptionDef = {
+  key: string,
+  value: string,
+  comment: string
+}
+
+export interface EnumDef {
+  key: string,
+  file: string
+  options: EnumOptionDef[]
+}
